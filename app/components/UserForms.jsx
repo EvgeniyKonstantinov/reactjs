c2c7dc73313582b71f97c8e66bb59b41eb106025
Evgeniy Konstantinov <Konstantinov-91@mail.ru>
2018-03-01 19:53:23 +1000

    v0.01

---
+++ app/components/UserForms.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+
+class UserForms extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {name: ''};
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(e) {
+        var  
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+module.exports = UserForms;
